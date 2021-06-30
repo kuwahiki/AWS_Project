03760f4c278244171ad846edb384060a76f987ba
a403 <a403@a403s-iMac.local>
2021-06-30 16:41:25 +0900

    20210630

--- Assets/MyAssets/Scripts/PlayUIController.cs
+++ Assets/MyAssets/Scripts/PlayUIController.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using Amazon;
 using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Internal;
 using Amazon.DynamoDBv2.Model;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.CognitoIdentity;
@@ -19,11 +20,21 @@ public class PlayUIController : MonoBehaviour
     private Button Send;
     AmazonDynamoDBClient Client;
     CognitoAWSCredentials Credentials;
-    int Bookid = 1001;
+    int Bookid = 1001,count = 0;
     DynamoDBContext Context;
+    bool getchat = false;
     // Start is called before the first frame update
     void Start()
     {
+        //AWSの設定
+        string region = AWSCognitoIDs.region;
+        string IdentityPoolId = AWSCognitoIDs.IdentityPoolId;
+        string UserPoolID = AWSCognitoIDs.UserPoolId;
+        var _CognitoPoolRegion = RegionEndpoint.GetBySystemName(AWSCognitoIDs.CognitoPoolRegion);
+        Credentials = new CognitoAWSCredentials(IdentityPoolId, _CognitoPoolRegion); // ID プールの ID を文字列で指定
+        var _DynamoRegion = RegionEndpoint.GetBySystemName(AWSCognitoIDs.DynamoRegion);
+        Client = new AmazonDynamoDBClient(Credentials,RegionEndpoint.USEast1); // ユーザープール使っていない、つまり未認証の権限ロールを使用することになります。
+        Context = new DynamoDBContext(Client); // アイテム操作は Context というものを介して操作する模様
 
         for (int i = 0; i < Icons.Length; i++)
         {
@@ -46,20 +57,11 @@ public class PlayUIController : MonoBehaviour
             trigger.triggers.Add(entry);
             //Debug.Log();
 
-            //AWSの設定
-            string region = AWSCognitoIDs.region;
-            string IdentityPoolId = AWSCognitoIDs.IdentityPoolId;
-            string UserPoolID = AWSCognitoIDs.UserPoolId;
-            var _CognitoPoolRegion = RegionEndpoint.GetBySystemName(AWSCognitoIDs.CognitoPoolRegion);
-            Credentials = new CognitoAWSCredentials(IdentityPoolId, _CognitoPoolRegion); // ID プールの ID を文字列で指定
-            var _DynamoRegion = RegionEndpoint.GetBySystemName(AWSCognitoIDs.DynamoRegion);
-            Client = new AmazonDynamoDBClient(Credentials,RegionEndpoint.USEast1); // ユーザープール使っていない、つまり未認証の権限ロールを使用することになります。
-            Context = new DynamoDBContext(Client); // アイテム操作は Context というものを介して操作する模様
-
             
+
+           
         }
-
-
+        getItemChat();
     }
 
 
@@ -97,8 +99,9 @@ public class PlayUIController : MonoBehaviour
     void SendChat(string chat)
     {
         //チャット内容をDynamoDBに送信する処理（AWS側の処理）
-        //PutItem(Client, chat);
-        Write();
+        PutItem(Client, chat);
+        //CreateTable(Client, "test");
+        count++;
 
     }
 
@@ -154,11 +157,12 @@ public class PlayUIController : MonoBehaviour
         //リクエストの構築
         PutItemRequest request = new PutItemRequest
         {
-            TableName = "UnityTest",//追加先のテーブル名
+            TableName = "UnityChat",//追加先のテーブル名
                                     //各カラムの値を指定
             Item = new Dictionary<string, AttributeValue>
                 {
-                    {"ThisIsId",new AttributeValue{S = chatText} },
+                    {"Name",new AttributeValue{S = "TestUser" + count.ToString()} },
+                    {"Chat",new AttributeValue{S = chatText } }
                 }
         };
         //テーブルに追加
@@ -166,29 +170,74 @@ public class PlayUIController : MonoBehaviour
         
 
     }
-    private void Write()
+
+    private void Update()
     {
-        //var bookBatch = context.CreateBatchWrite<Book>();
-
-        //Book book1 = new Book
-        //{
-        //    Test = chatText
-        //};
-
-        //bookBatch.AddPutItem(book1);
-        //DynamoDBContext context, string chatText
-
-
-        Book myBook = new Book
+        if(getchat == false)
         {
-            Id = 1001,
-            NewValue = "object persistence-AWS SDK for.NET SDK-Book 1001",
-            //ISBN = "111-1111111001",
-            //BookAuthors = new List<string> { "Author 1", "Author 2" },
+           //getItemChat();
+        }
+
+    }
+
+    private async Task getItemChat()
+    {
+        GetItemRequest request = new GetItemRequest
+        {
+            Key = new Dictionary<string, AttributeValue>() { { "Name", new AttributeValue {S = "TestUser"} } },
+            TableName = "UnityChat"
         };
-        // Save the book.
-        CancellationToken result;
-        Context.SaveAsync(myBook,result);
+
+        if(request == null)
+        {
+            Debug.Log("null");
+        }
+        try {
+            //var res = await Client.GetItemAsync(request);
+            var req = new ScanRequest("UnityChat");
+            var res = await Client.ScanAsync(req);
+            Text  text= GameObject.Find("Canvas/Panel/Text").GetComponent<Text>();
+            var items = res.Items;
+            //PrintChat(text,res.Items); 
+        }
+        catch(Exception ex)
+        {
+            Debug.LogError(ex);
+        }
+        Debug.Log("test");
+
+    }
+
+    private void PrintChat(Text ChatCanvas,IDictionary<string, AttributeValue> attributeList)
+    {
+        Debug.Log("chatprint");
+        int i = 0;
+        string Username = "", chat = "";
+        foreach (KeyValuePair<string,AttributeValue> kvp in attributeList)
+        {
+            string attributeName = kvp.Key;
+            AttributeValue Value = kvp.Value;
+
+            //Debug.Log(attributeName +"," +
+            //    (Value.S == null ? "" : "S=[" + Value.S + "]") +
+            //        (Value.N == null ? "" : "N=[" + Value.N + "]") +
+            //        (Value.SS == null ? "" : "SS=[" + string.Join(",", Value.SS.ToArray()) + "]") +
+            //        (Value.NS == null ? "" : "NS=[" + string.Join(",", Value.NS.ToArray()) + "]")
+            //    );
+            if(i%2 == 0)
+            {
+                chat = Value.S;
+            }
+            else
+            {
+                Username = Value.S;
+                ChatCanvas.text = Username + ":" + chat + "\n";
+            }
+            i++;
+        }
+
+        Debug.Log(ChatCanvas.text);
+
     }
 }
 
